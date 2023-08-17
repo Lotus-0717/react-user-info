0fddc99fc970201d3d0deb69aa696d79cbcd23c9
lotus <lian82717pro@gmail.com>
2023-08-17 23:51:20 +0800

    add home list

---
+++ src/pages/Home/components/list.js
@@ -0,0 +1,11 @@
+import Item from "./item";
+
+export default function List() {
+  return(
+    <ul>
+      <Item></Item>
+      <Item></Item>
+      <Item></Item>
+    </ul>
+  )
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,11 @@
+import List from "./components/list";
+import '../../scss/home.scss';
 export default function Home() {
   return(
     <section className="page">
       <h2>Home</h2>
       <hr></hr>
+      <List></List>
     </section>
   )
 }
